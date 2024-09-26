4cdd406da07d24c39c0fb00004859bb224511ad1
bagdatkamila <kamillabagdat@gmail.com>
2024-09-26 23:50:58 +0500

    Initial Commit

---
+++ I am rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I am rich
+//
+//  Created by Камила Багдат on 26.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I am rich/I_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_richApp.swift
+//  I am rich
+//
+//  Created by Камила Багдат on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct I_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
